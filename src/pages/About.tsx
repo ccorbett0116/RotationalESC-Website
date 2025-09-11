3fe51a13f89be5ebec2c8a1daa6339011c62ece5
Cole <cole@colecorbett.ca>
2025-09-11 17:05:38 -0400

    vendors

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -128,8 +128,8 @@ const About = () => {
               <div className="w-16 h-16 bg-primary/10 rounded-full flex items-center justify-center mx-auto mb-4">
                 <Award className="h-8 w-8 text-primary" />
               </div>
-              <h3 className="text-3xl font-bold text-foreground mb-2">PLACEHOLDER</h3>
-              <p className="text-muted-foreground">PLACEHOLDER</p>
+              <h3 className="text-3xl font-bold text-foreground mb-2">Vendors</h3>
+              <p className="text-muted-foreground">Authorized Representative to Several Key Manufacturers</p>
             </div>
           </div>
         </div>
